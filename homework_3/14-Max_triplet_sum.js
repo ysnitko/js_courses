52ce1a597f3e5bf428ebe7f0aa3d5e5ff39a3c56
yuri_sn <yuri.snitko@gmail.com>
2023-03-10 00:12:34 +0300

    js task

---
+++ homework_3/14-Max_triplet_sum.js
@@ -0,0 +1,34 @@
+function maxTriSum(numbers) {
+  let result = [];
+  let sum = 0;
+  function sortArr(x, y) {
+    if (x < y) {
+      return 1;
+    }
+    if (x > y) {
+      return -1;
+    }
+    if (x == y) {
+      return 0;
+    }
+  }
+
+  function delRepeat(numbers) {
+    let tempArr = [];
+    for (let index = 0; index < numbers.length; index++) {
+      if (!tempArr.includes(numbers[index])) {
+        tempArr.push(numbers[index]);
+      }
+    }
+    return tempArr;
+  }
+
+  result = delRepeat(numbers).sort(sortArr).slice(0, 3);
+  for (let index = 0; index < result.length; index++) {
+    sum += result[index];
+  }
+  return sum;
+}
+
+console.log(maxTriSum([3, 2, 6, 8, 2, 3]));
+console.log(maxTriSum([-7, 12, -7, 29, -5, 0, -7, 0, 0, 29]));
